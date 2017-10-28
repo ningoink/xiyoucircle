f1d23fbee2a228ebc3c46fe6aad2ac291f84ffad
hylerrix <hylerrix@gmail.com>
2017-10-28 16:33:04 +0800

    :lipstick: updating page one`s UI and its style

--- src/script/entryjs/index.js
+++ src/script/entryjs/index.js
@@ -1,2 +1,14 @@
 import '../../image/earlyBirds.jpg'
+import '../../image/qqGroupQR.jpg'
 import '../../style/index.css'
+import '../../style/indexPage1.css'
+
+import joinUs from '../commonjs/joinUs.js'
+var openJoinQR = joinUs.openJoinQR
+var closeJoinQR = joinUs.closeJoinQR
+
+let openJoinQRButton = document.getElementById('openJoinQR')
+openJoinQRButton.onclick = openJoinQR
+
+let closeJoinQRButton = document.getElementById('closeJoinQR')
+closeJoinQRButton.onclick = closeJoinQR
