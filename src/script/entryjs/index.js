a894fdcfa3058495232e7e550505b0dddd4bdbc9
icorvoh <icorvoh@qq.com>
2017-10-31 18:22:14 +0800

    :sparkles: home page almost done

--- src/script/entryjs/index.js
+++ src/script/entryjs/index.js
@@ -1,6 +1,9 @@
 import '../../image/earlyBirds.png'
 import '../../image/qqGroupQR.jpg'
 import '../../image/temp.png'
+import '../../image/laboratory/creatshare.png'
+import '../../image/laboratory/epi-logo.png'
+
 import '../../style/index.css'
 import '../../style/indexCard1.css'
 import '../../style/indexCard2.css'
