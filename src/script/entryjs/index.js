e50ffd7001a3e18b2abf1cb1568442473ee7bf98
hylerrix <hylerrix@gmail.com>
2017-11-05 15:52:07 +0800

    :sparkles: let`s make this online

--- src/script/entryjs/index.js
+++ src/script/entryjs/index.js
@@ -2,7 +2,7 @@ import '../../image/earlyBirds.png'
 import '../../image/qqGroupQR.jpg'
 import '../../image/temp.png'
 import '../../image/laboratory/creatshare.png'
-import '../../image/laboratory/epi-logo.png'
+import '../../image/laboratory/epi-logo.jpg'
 
 import '../../style/index.css'
 import '../../style/indexCard1.css'
