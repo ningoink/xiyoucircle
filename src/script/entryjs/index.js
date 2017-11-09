cd47b3c49f63a362c4e3c8c5d5619db0450af469
Demoiselle7 <yuekewang7@gmail.com>
2017-11-09 11:59:57 +0800

    submit
    add 2+2 logo

--- src/script/entryjs/index.js
+++ src/script/entryjs/index.js
@@ -3,6 +3,8 @@ import '../../image/qqGroupQR.jpg'
 import '../../image/temp.png'
 import '../../image/laboratory/creatshare.png'
 import '../../image/laboratory/epi-logo.jpg'
+import '../../image/laboratory/22.jpg'
+
 
 import '../../style/index.css'
 import '../../style/indexCard1.css'
