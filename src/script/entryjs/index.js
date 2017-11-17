490a05ea32b84fdcd199427132ccb240d9509289
hylerrix <hylerrix@gmail.com>
2017-11-18 00:38:53 +0800

    :face_with_thermometer: messed code for re-build index page

--- src/script/entryjs/index.js
+++ src/script/entryjs/index.js
@@ -1,27 +1,28 @@
 import '../../image/earlyBirds.png'
 import '../../image/qqGroupQR.jpg'
-import '../../image/temp.png'
-import '../../image/laboratory/creatshare.png'
-import '../../image/laboratory/epi-logo.jpg'
 
+import '../../image/index/lab.png'
+import '../../image/index/info.png'
+import '../../image/index/roadmap.png'
+import '../../image/index/leader.png'
+import '../../image/index/about.png'
+
+import '../../image/lab/creatshare.png'
+import '../../image/lab/epi-logo.jpg'
+
+import '../../style/_reset.css'
 import '../../style/index.css'
 import '../../style/indexCard1.css'
 import '../../style/indexCard2.css'
 import '../../style/indexCard3.css'
 
 import joinUs from '../commonjs/joinUs.js'
-import background from '../commonjs/ranBackground.js'
 
 var openJoinQR = joinUs.openJoinQR
 var closeJoinQR = joinUs.closeJoinQR
-var ranBackground = background.ranBackground
 
 let openJoinQRButton = document.getElementById('openJoinQR')
 openJoinQRButton.onclick = openJoinQR
 
 let closeJoinQRButton = document.getElementById('closeJoinQR')
 closeJoinQRButton.onclick = closeJoinQR
-
-window.onload=function(){
-    ranBackground()
-}
