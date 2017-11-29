1265b66d360ec7ba74c5f399dc490f78ea2a5580
hylerrix <hylerrix@foxmail.com>
2017-11-29 11:39:23 +0800

    :hammer: refactoring lab page...

--- src/script/entryjs/lab.js
+++ src/script/entryjs/lab.js
@@ -2,5 +2,6 @@ import '../../image/icon/earlyBirds.png'
 
 import '../../image/lab/creatshare-logo.jpg'
 
+import '../../style/_reset.css'
 import '../../style/lab.css'
 import '../../style/animate.css'
