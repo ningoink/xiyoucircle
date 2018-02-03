9912a0d03a026f1b243f8de7daa7326bdbfb67d6
Demoiselle7 <yuekewang7@gmail.com>
2018-02-03 16:59:59 +0800

    dev of about commit
    init the project

---
+++ src/script/entryjs/about.js
@@ -0,0 +1,13 @@
+import '../../image/about/git.png'
+import '../../image/about/spine.png'
+
+
+import '../../image/about/people1.jpg'
+import '../../image/about/people2.jpg'
+import '../../image/about/people3.jpg'
+import '../../image/about/people4.jpg'
+import '../../image/about/people5.jpg'
+import '../../image/about/people6.jpg'
+
+
+import '../../style/about.css'
--- src/script/entryjs/lab.js
+++ src/script/entryjs/lab.js
@@ -2,6 +2,5 @@ import '../../image/icon/earlyBirds.png'
 
 import '../../image/lab/creatshare-logo.jpg'
 
-import '../../style/_reset.css'
 import '../../style/lab.css'
 import '../../style/animate.css'
