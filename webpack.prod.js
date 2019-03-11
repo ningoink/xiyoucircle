8eb777be82ef976ef282bcc1e7735982f93cadb8
hylerrix <hylerrix@gmail.com>
2019-03-11 16:55:52 +0800

    引入 vue cli 3 项目基础骨架，成功运行初始页面

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-const commonConfig = require('./webpack.config.js')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-
-module.exports = merge(commonConfig, {
-  plugins: [
-    new UglifyJSPlugin()
-  ]
-})
