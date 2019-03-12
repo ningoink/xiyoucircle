1362ec93199ceaa58fb77814044dc51b0c3c36eb
hylerrix <hylerrix@gmail.com>
2019-03-12 15:30:46 +0800

    编写一部分通用组件

---
+++ src/components/app-footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="footer">
+        Copyright © 2017-2018 <a href="https://github.com/ningowood/xiyoucircle/" target="_blank">西邮开源社</a>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<style scoped>
+.footer {
+    position: relative;
+    bottom: 0;
+    line-height: 10vh;
+    text-align: center;
+    height: 10vh;
+    width: 100vw;
+    background-color: #242424;
+}
+
+.footer a {
+    color: #fff;
+}
+</style>
---
+++ src/components/app-nav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'AppNav'
+}
+</script>
+
+<style scoped>
+
+</style>
+
