e1d0b5dafc50585305e4414bb071fa9a6fbfea46
hylerrix@foxmail.com <hylerrix@foxmail.com>
2019-03-12 15:32:45 +0800

    抽离首页子组件

---
+++ src/views/home/card-1.vue
@@ -0,0 +1,138 @@
+<template>
+    <section id="card1" class="card">
+        <div class="card1Wrap">
+            <p>
+                <img src="@/assets/icon/earlyBirds.png" class="logo" alt="logo">
+            </p>
+            <h1 id="xiyoucircle">
+                <a href="#" class="anchor">
+                    <span>西邮开源社<small>0.0.1</small></span>
+                </a>
+            </h1>
+            <blockquote>
+            <p>咱西邮人自己的开源技术圈。</p>
+            </blockquote>
+            <ul>
+                <li>开源技术意味着什么？</li>
+                <li>我们都要掌握哪些技能？</li>
+                <li>有哪些实验室我可以加入？</li>
+                <li>一切尽在西邮开源社！</li>
+            </ul>
+            <p>
+                <a href="https://github.com/ningowood/xiyoucircle/" target="_blank">Github</a>
+                <a href="#" id="openJoinQR" v-on:click="openJoinQR">加入我们</a>
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Card1',
+    data: function () {
+        return {
+            qrDialog: false
+        }
+    },
+    methods: {
+        openJoinQR: function () {
+            // this.qrDialog = true
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+#card1 {
+    height: 80vh;
+    background-image: url('../../assets/index/indexBackground.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+}
+
+#card1 .card1Wrap {
+    -webkit-box-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+    margin: -20px 16px 0;
+    text-align: center;
+    z-index: 1;
+}
+
+#card1 .logo {
+    height: 10rem;
+    width: 10rem;
+}
+
+#card1 h1 {
+    color: #fff;
+    font-size: 2.5rem;
+    font-weight: 300;
+    margin: .625rem 0 2.5rem;
+    position: relative;
+    text-align: center;
+}
+
+#card1 h1 a {
+    display: block;
+}
+
+#card1 a, #card1 a:hover {
+    text-decoration: none;
+}
+
+#card1 .anchor span {
+    color: #fff;
+}
+
+#card1 h1 small {
+    bottom: -7px;
+    bottom: -.4375rem;
+    font-size: 1rem;
+    position: absolute;
+}
+
+#card1 blockquote {
+    font-size: 1.5rem;
+    text-align: center;
+}
+
+#card1 p {
+    line-height: 1.5rem;
+}
+
+#card1 ul {
+    line-height: 1.8;
+    list-style-type: none;
+    margin: 1em auto;
+    max-width: 500px;
+    padding: 0;
+}
+
+#card1 .card1Wrap>p:last-child a:last-child {
+    background-color: #42b983;
+    color: #fff;
+    margin-right: 0;
+}
+
+#card1 .card1Wrap>p:last-child a {
+    border-color: #42b983;
+    border: 2px solid #42b983;
+    border-radius: 2rem;
+    box-sizing: border-box;
+    color: #42b983;
+    background-color: #fff;
+    display: inline-block;
+    font-size: 1rem;
+    letter-spacing: .1rem;
+    margin-right: 1rem;
+    padding: .75em 2rem;
+    text-decoration: none;
+    transition: all .15s ease;
+}
+</style>
---
+++ src/views/home/card-3.vue
@@ -0,0 +1,76 @@
+<template>
+    <section id="card3" class="card">
+        <div class="card3Wrap">
+            <div class="cardHeaderWrap">
+                <div class="cardHeader">期待你们的加盟</div>
+                <div class="cardHorizontalLine"><span>—</span></div>
+                <div class="cardSubheader">助力西邮各大西邮实验室展现自我</div>
+            </div>
+            <div class="labWrap">
+                <div class="labItem"><img src="@/assets/lab/creatshare.png" alt="CreatShare互联网实验室"/></div>
+                <div class="labItem"><img src="@/assets/lab/epi-logo.jpg" alt="EPI电子实验室"/></div>
+                <div class="labItem"><img src="@/assets/lab/22.jpg" alt="2+2实验室"/></div>
+                <div class="labItem"><img src="@/assets/icon/hands.png" alt="申请加盟"/></div>
+                <div class="labItem"><img src="@/assets/icon/more.png" alt="更多实验室"/></div>
+            </div>
+            <div class="labApplyFor">
+                <a href="https://github.com/ningowood/xiyoucircle/issues/10" target="_blank">代表你的实验室申请加盟 ></a>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Card3'
+}
+</script>
+
+<style scoped>
+
+#card3 {
+    background: #fff;
+    min-height: 200px;
+    max-height: 1600px;
+}
+
+.card3Wrap {
+    margin: 0 15px 0;
+    text-align: center;
+}
+
+.labWrap {
+    margin: 0 auto;
+    max-width: 1200px;
+    overflow: hidden;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.labItem {
+    margin: 15px 10px 15px 10px;
+    flex: 1;
+    height: 60px;
+    min-width: 200px;
+    max-width: 200px;
+    display: flex;
+    align-items: center;
+}
+
+.labItem img {
+    max-height: 60px;
+    margin: 0 auto;
+    max-width: 188px;
+}
+
+.labApplyFor {
+    margin: 2rem;
+    padding-top: .4rem;
+}
+
+.labApplyFor a {
+    color: #36a2ff;
+}
+</style>
+
