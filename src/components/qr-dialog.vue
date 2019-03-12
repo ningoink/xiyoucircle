a22dcef02fd046a616d4b89417109ac4cc39fb08
hylerrix@foxmail.com <hylerrix@foxmail.com>
2019-03-12 15:30:46 +0800

    编写一部分通用组件

---
+++ src/components/qr-dialog.vue
@@ -0,0 +1,74 @@
+<template>
+    <!-- 加入我们，扫码入 QQ 群，弹出框 -->
+    <div id="dialog">
+        <div id="qqGroupQR">
+            <!-- <a href="#" id="closeJoinQR" v-on:click="closeJoinQR">长按图片扫码，点我返回</a> -->
+            <a href="#" id="closeJoinQR" v-on:click="closeJoinQR">长按图片扫码，点我返回</a>
+            <img src="@/assets/index/qqGroupQR.jpg" alt="QQ群">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QrDialog',
+    methods: {
+        closeJoinQR: function () {
+            // this.$emit = false
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+#dialog {
+    /* display: none; */
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: 1111;
+}
+
+#qqGroupQR {
+    position: fixed;
+    top: 1.5rem;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    margin: 0px auto;
+    height: 20rem;
+    text-align: center;
+}
+
+#dialog img {
+    display: block;
+    margin: 30px auto;
+}
+
+#dialog img {
+    width: 100%; /* 36rem */
+    height: 32.5rem; /* 50rem */
+}
+
+#closeJoinQR {
+    display: inline-block;
+    border: 2px solid orange;
+    border-radius: 2rem;
+    border-color: orange;
+    box-sizing: border-box;
+    letter-spacing: .1rem;
+    transition: all .15s ease;
+    text-decoration: none;
+    color: #fff;
+    background-color: orange;
+    height: 3rem;
+    width: 20rem;
+    text-align: center;
+    font-size: 1.2rem;
+    padding-top: .6rem;
+}
+</style>
+
