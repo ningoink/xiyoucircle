dee6ea549985048b57ee42381f0ec8d79a3778cc
WolfGirlM <18829491124@163.com>
2019-03-15 02:05:52 +0800

    搜索框

--- src/components/lab-search.vue
+++ src/components/lab-search.vue
@@ -1,11 +1,15 @@
 <template>
+<div>
     <p>实验室搜索</p>
     <input type="text" placeholder="输入实验室名称">
-  <form>
+    <form>
       <label><input  type="checkbox" value="" />东区 </label>
       <label><input  type="checkbox" value="" />西区 </label>
       <label><input  type="checkbox" value="" />老校区 </label>
   </form>
+
+</div>
+    
 </template>
 
 <script>
