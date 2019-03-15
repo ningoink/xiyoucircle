46adcafafc3542667a9ebf1aefd740ca515ca6ae
WolfGirlM <18829491124@163.com>
2019-03-15 17:05:11 +0800

    需要重构的响应式的布局

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,33 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app" >
+    <!-- 根据弹出框显示状态，给每个页面根元素加入模糊效果并跟踪相关点击事件 -->
+    <router-view 
+      v-bind:class="{ filter: qrIsShow }">
+    </router-view>
+    <qr-dialog v-if="qrIsShow"></qr-dialog>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import QrDialog from '@/components/qr-dialog'
+import Home from '@/views/home/index.vue'
+
+import '@/style/reset.css'
+import '@/style/card.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QrDialog,
+    Home
   },
-  data () {
-    return {
-      //
+  computed: {
+    qrIsShow () {
+      return this.$store.state.qrDialog.opened
     }
   }
 }
 </script>
+
+<style>
+</style>
