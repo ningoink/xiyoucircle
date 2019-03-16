284222e50ca3e8ea193af9f011c67bad405d0a26
WolfGirlM <18829491124@163.com>
2019-03-16 13:19:47 +0800

    终局之战-野味狼果

--- src/config/lab-detail.js
+++ src/config/lab-detail.js
@@ -26,5 +26,40 @@ export const LAB_DETAIL = [
     direction: '产品经理、产品运营、视觉设计、前端开发、后端开发',
     from: '计算机学院',
     date: '2012.02.14'
+  },
+  {
+    id: 5,
+    name: 'Createshare实验室',
+    direction: '产品经理、产品运营、视觉设计、前端开发、后端开发',
+    from: '计算机学院',
+    date: '2012.02.14'
+  },
+  {
+    id: 6,
+    name: 'Createshare实验室',
+    direction: '产品经理、产品运营、视觉设计、前端开发、后端开发',
+    from: '计算机学院',
+    date: '2012.02.14'
+  },
+  {
+    id: 7,
+    name: 'Createshare实验室',
+    direction: '产品经理、产品运营、视觉设计、前端开发、后端开发',
+    from: '计算机学院',
+    date: '2012.02.14'
+  },
+  {
+    id: 8,
+    name: 'Createshare实验室',
+    direction: '产品经理、产品运营、视觉设计、前端开发、后端开发',
+    from: '计算机学院',
+    date: '2012.02.14'
+  },
+  {
+    id: 9,
+    name: 'Createshare实验室',
+    direction: '产品经理、产品运营、视觉设计、前端开发、后端开发',
+    from: '计算机学院',
+    date: '2012.02.14'
   }
 ]
