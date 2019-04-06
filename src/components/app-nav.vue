355488a33d41e406f2c63f55ae5d461092832965
AqingCyan <xuekaiqi@creatshare.com>
2019-04-06 15:50:51 +0800

    nav对应路由点亮

--- src/components/app-nav.vue
+++ src/components/app-nav.vue
@@ -3,32 +3,56 @@
     <header class="header">
       <router-link to="/" class="header__logo">西邮实验室</router-link>
       <nav class="header__nav">
-        <router-link
-          to="/lab"
-          class="header__nav-item header__nav-item_status_active header__nav-item_i_1"
-        >西邮实验室</router-link>
-        <router-link
-          to="/about"
-          class="header__nav-item header__nav-item_status_active header__nav-item_i_1"
-        >关于开源社</router-link>
+        <span @click="handleSwitch">
+          <router-link
+            to="/lab"
+            class="header__nav-item header__nav-item_status_active header__nav-item_i_1"
+          >西邮实验室</router-link>
+        </span>
+        <span @click="handleSwitch">
+          <router-link
+            to="/about"
+            class="header__nav-item header__nav-item_status_active header__nav-item_i_1"
+          >关于开源社</router-link>
+        </span>
         <a
           href="#"
           id="openJoinQR"
           v-on:click="toggleQrDialog"
           class="header__nav-item header__nav-item_custom_button"
         >加入我们</a>
-        <!-- <div class="header__nav-item-tip"></div> 底部选中状态展示 -->
+        <div :class="bottom_class"></div>
       </nav>
     </header>
   </div>
 </template>
 
 <script>
+import { watchFile } from "fs";
 export default {
+  data() {
+    return {
+      bottom_class: "header__nav-item-tip_left"
+    };
+  },
   name: "AppNav",
   methods: {
     toggleQrDialog() {
       this.$store.dispatch("qrDialog/toggleQrDialog");
+    },
+    handleSwitch() {
+      if (this.$route.fullPath === "/lab") {
+        this.bottom_class = "header__nav-item-tip_left";
+      } else if (this.$route.fullPath === "/about") {
+        this.bottom_class = "header__nav-item-tip_right";
+      }
+    }
+  },
+  mounted() {
+    if (this.$route.fullPath === "/lab") {
+      this.bottom_class = "header__nav-item-tip_left";
+    } else if (this.$route.fullPath === "/about") {
+      this.bottom_class = "header__nav-item-tip_right";
     }
   }
 };
@@ -94,12 +118,21 @@ a:hover {
   line-height: 40px;
 }
 
-.header__nav-item-tip {
+.header__nav-item-tip_left {
   width: 80px;
   height: 2px;
   background: #20a0ff;
   position: absolute;
-  margin-left: 45px;
+  margin-left: 35px;
+  bottom: -2px;
+}
+
+.header__nav-item-tip_right {
+  width: 80px;
+  height: 2px;
+  background: #20a0ff;
+  position: absolute;
+  margin-left: 185px;
   bottom: -2px;
 }
 
@@ -116,5 +149,8 @@ a:hover {
 .header__nav-item_custom_button:hover {
   color: #ffffff;
 }
+
+/*响应式*/
+@media only screen and (max-width: 768px) {}
 </style>
 
